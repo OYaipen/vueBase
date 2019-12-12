b8e400cd248e0fdf753246db165a9b97eb901bda
Owen Yaipen <1999yaipen@gmail.com>
2019-12-12 13:57:08 -0500

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,17 @@
+const path = require("path");
+
 module.exports = {
     devServer: {
         port: 3000
     },
-    outputDir: './client',
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '~': __dirname + '/vue/'
-            }
-        },
-        entry: {
-            app: './vue/app.js'
-        }
+    outputDir: './dist',
+    chainWebpack: config => {
+        config
+            .entry("app")
+            .clear()
+            .add("./vue/app.js")
+            .end();
+        config.resolve.alias
+            .set("~", path.join(__dirname, "./vue"))
     }
-}
+};
